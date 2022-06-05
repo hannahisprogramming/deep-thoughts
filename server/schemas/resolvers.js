d53ea897ed66acb008b12908abb71429c1bad81c
Hannah Bush <heb.rutgers@gmail.com>
2022-06-05 12:20:17 -0400

    added addUser, login, addThought, addReaction, addFriend

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,13 +1,25 @@
-const {User, Thought} = require('../models');
+const { User, Thought } = require('../models');
+const { signToken } = require('../utils/auth')
+const { AuthenticationError } = require('apollo-server-express');
 
 const resolvers = {
   Query: {
-    thoughts: async (parent, {username}) => {
-      const params = username ? {username} : {};
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({})
+          .select(-__v - password)
+          .populate('thoughts')
+          .populate('friends');
+        return userData;
+      }
+      throw new AuthenticationError('Not logged in');
+    },
+    thoughts: async (parent, { username }) => {
+      const params = username ? { username } : {};
       return Thought.find(params).sort({ createdAt: -1 });
     },
-    thought: async (parent, {_id}) => {
-      return Thought.findOne({_id});
+    thought: async (parent, { _id }) => {
+      return Thought.findOne({ _id });
     },
     users: async () => {
       return User.find()
@@ -15,11 +27,72 @@ const resolvers = {
         .populate('friends')
         .populate('thoughts');
     },
-    user: async (parent, {username}) => {
-      return User.findOne({username})
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
         .select('-__v -password')
         .populate('friends')
         .populate('thoughts');
+    }
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect username');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect password');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    addThought: async (parent, args, context) => {
+      if (context.user) {
+        const thought = await Thought.create({ ...args, username: context.user.username});
+
+        await User.findByIdAndUpdate(
+          { _id: context.user._id},
+          { $push: { thoughts: thought._id}},
+          { new: true }
+        );
+        return thought;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    addReaction: async (parent, { thoughtId, reactionBody }, context) => {
+      if (context.user) {
+        const updatedThought = await Thought.findOneAndUpdate(
+          { _id: thoughtId },
+          { $push: { reactions: { reactionBody, username: context.user.username } } },
+          { new: true, runValidators: true }
+        );
+    
+        return updatedThought;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    addFriend: async (parent, { friendId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { friends: friendId } },
+          { new: true }
+        ).populate('friends');
+    
+        return updatedUser;
+      }
+      throw new AuthenticationError('You need to be logged in!');
     }
   }
 };
